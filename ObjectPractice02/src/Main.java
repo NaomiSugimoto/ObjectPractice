b9146681e502ea26f20261a2c7e773182f8a7bda
Naomi Sugimoto <>
2018-03-25 11:12:15 +0900

    課題を提出いたします。

---
+++ ObjectPractice02/src/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args) {
+
+        Circle circle  = new Circle();
+
+        circle.getArea();
+        circle.getCircumference();
+
+        System.out.println(circle.getArea());
+        System.out.println(circle.getCircumference());
+
+    }
+}
